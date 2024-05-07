9c47f0bfae68f06dc0b84400561de4b62cbb9e5a
Utsav Vasoya <utsavvasoya89@gmail.com>
2024-05-07 12:44:23 +0530

    add gitignore

--- controller/checkoutController.js
+++ controller/checkoutController.js
@@ -1,6 +1,8 @@
 const stripeKey = "YOUR_TOKEN";
 const domain = "http://localhost:3001/";
-const stripe = require("stripe")(stripeKey);
+const stripe = require("stripe")(
+  "sk_test_51OUkJ5SEn0ZRdGTviyN4tSTGSA01RLA6gg9o5tnyCWq8ERAGzCyIpjwFizBXuJmj234IRsQTzWHXRdvzaaYCkFWt00s6enWnLY"
+);
 
 const getSessionData = async (req, res) => {
   try {
@@ -8,21 +10,21 @@ const getSessionData = async (req, res) => {
       amount: req.body.price * 100,
       currency: "usd",
       description: req.body.description,
-      shipping: {
-        name: "Jenny Rosen",
-        address: {
-          line1: "510 Townsend St",
-          postal_code: "98140",
-          city: "San Francisco",
-          state: "CA",
-          country: "US",
-        },
-      },
-      metadata: {
-        currency: "usd",
-        id: "Asasas",
-      },
-      receipt_email: req.body.email,
+      // shipping: {
+      //   name: "Jenny Rosen",
+      //   address: {
+      //     line1: "510 Townsend St",
+      //     postal_code: "98140",
+      //     city: "San Francisco",
+      //     state: "CA",
+      //     country: "US",
+      //   },
+      // },
+      // metadata: {
+      //   currency: "usd",
+      //   id: "Asasas",
+      // },
+      // receipt_email: req.body.email,
       payment_method_types: ["card"],
     });
 
